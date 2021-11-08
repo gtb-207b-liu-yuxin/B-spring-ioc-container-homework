4cb2b74ffb28ea6488206e79a60aa40094b83353
Yuxin <yuxin.liu1@thoughtworks.com>
2021-11-08 21:01:20 +0800

    feat: [Yuxin] make Foo implement ApplicationContextAware and InitializingBean to solve circular dependency issues

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,14 +1,19 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Foo {
+public class Foo implements ApplicationContextAware, InitializingBean {
 
     private Bar bar;
+    private ApplicationContext context;
 
-    public Foo(Bar bar) {
-        this.bar = bar;
+    public Bar getBar() {
+        return bar;
     }
 
     public void hi() {
@@ -17,5 +22,15 @@ public class Foo {
 
     public String name() {
         return "Foo";
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        bar = context.getBean(Bar.class);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
     }
 }
