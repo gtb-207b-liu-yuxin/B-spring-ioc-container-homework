7b246ba35ee765c8cc108e116d48510f00c638e4
Yuxin <yuxin.liu1@thoughtworks.com>
2021-11-08 20:54:10 +0800

    feat: [Yuxin] use @PostConstruct annotation to solve the circular dependency problem

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -7,8 +7,12 @@ public class Bar {
 
     private Foo foo;
 
-    public Bar(Foo foo) {
+    public void setFoo(Foo foo) {
         this.foo = foo;
+    }
+
+    public Foo getFoo() {
+        return foo;
     }
 
     public void hi() {
