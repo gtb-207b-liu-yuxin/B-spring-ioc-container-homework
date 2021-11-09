80ffa0c62254b74d492183d8c713fa2c99ce85bc
Yuxin <yuxin.liu1@thoughtworks.com>
2021-11-09 15:27:06 +0800

    feat: [Yuxin] using object factory to make service layer scope to be prototype

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,23 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class GreetingController {
 
-    private final GreetingService greetingService;
-
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
+    private ObjectFactory<GreetingService> greetingService;
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        System.out.println(greetingService.getObject());
+        return greetingService.getObject().sayHi();
     }
 
 }
